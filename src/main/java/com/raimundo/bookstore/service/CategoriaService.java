868d27028f428a2c6729ccd6a84200faf6934dd7
ralmeidaj <ralmeidaj@gmail.com>
2022-04-26 16:57:58 -0300

    Delete para categoria

--- src/main/java/com/raimundo/bookstore/service/CategoriaService.java
+++ src/main/java/com/raimundo/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.raimundo.bookstore.domain.Categoria;
@@ -39,6 +40,11 @@ public class CategoriaService {
 		obj.setDescricao(objDTO.getDescricao());
 		return repository.save(obj);
 		
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		repository.deleteById(id);
 	}
 	
 }

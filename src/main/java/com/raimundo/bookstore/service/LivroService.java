80eb5e16898f5848741dc670d06cf28f1a1c48dd
ralmeidaj <ralmeidaj@gmail.com>
2022-04-26 17:31:51 -0300

    Get para Livro por id

---
+++ src/main/java/com/raimundo/bookstore/service/LivroService.java
@@ -0,0 +1,25 @@
+package com.raimundo.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.raimundo.bookstore.domain.Livro;
+import com.raimundo.bookstore.repositories.LivroRepository;
+import com.raimundo.bookstore.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class LivroService {
+
+	@Autowired
+	private LivroRepository repository;
+	
+	
+	public Livro findById(Integer id) {
+		
+		Optional<Livro>obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + "Tipo: " + Livro.class.getName()));
+		
+	}
+}

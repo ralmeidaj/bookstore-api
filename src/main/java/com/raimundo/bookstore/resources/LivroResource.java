e6fc8201a2d1e821681b8e07565a441e172c5e23
ralmeidaj <ralmeidaj@gmail.com>
2022-04-27 11:13:13 -0300

    Listando todas os livros por categoria

--- src/main/java/com/raimundo/bookstore/resources/LivroResource.java
+++ src/main/java/com/raimundo/bookstore/resources/LivroResource.java
@@ -1,13 +1,19 @@
 package com.raimundo.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.raimundo.bookstore.domain.Livro;
+import com.raimundo.bookstore.dtos.LivroDTO;
 import com.raimundo.bookstore.service.LivroService;
 
 @RestController
@@ -23,4 +29,13 @@ public class LivroResource {
 		return ResponseEntity.ok().body(obj);
 				
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0" ) Integer id_cat){
+		List<Livro> list = service.findAll(id_cat);
+		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	
 }

7efb9d60b12725332272a24a9e3286fbbc350fc8
ralmeidaj <ralmeidaj@gmail.com>
2022-04-27 13:54:15 -0300

    Create do Livro

--- src/main/java/com/raimundo/bookstore/service/LivroService.java
+++ src/main/java/com/raimundo/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.raimundo.bookstore.domain.Categoria;
 import com.raimundo.bookstore.domain.Livro;
 import com.raimundo.bookstore.repositories.LivroRepository;
 import com.raimundo.bookstore.service.exceptions.ObjectNotFoundException;
@@ -48,5 +49,13 @@ public class LivroService {
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
 	
+	}
+
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 }
